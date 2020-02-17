d6c17de5211f5a3835f9193f7082ae469c5e268f
samtou <christophe1999da@gmail.com>
2020-02-17 19:12:28 +0000

    fetching SECRET_KEY from environment variables

--- dashmed_heroku/settings.py
+++ dashmed_heroku/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_h=x0c27)j=g*b1f5!w6+eq^lqw@)j%jo6f98+fm@%svc@yxn@'
+SECRET_KEY = os.environ.get("SECRET_KEY", "SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
