146186664e75c1b5f42b83af039e1de0df5473cb
samtou <christophe1999da@gmail.com>
2020-03-17 01:53:13 +0000

    *methodes pourcentage_rsi, pourcentage, retour_sur_investissement, date_finet rang *refresh_status_payements en commentaire

--- investissement/models.py
+++ investissement/models.py
@@ -1,12 +1,14 @@
+from datetime import date
+
 from django.db import models
 
-from datetime import date, datetime
-
+from compte.models import Compte
 from investissement.utils import incrementer_date
 from investisseur.models import Investisseur
-from compte.models import Compte
 
-POURCENTAGE = 0.05
+POURCENTAGE_BONUS = .05
+POURCENTAGE_MENSUEL = .4
+MONTH_LENGTH = 30
 TYPE_CHOICES = [
     ('B', 'Bloqué'),
     ('M', 'Mensuel')
@@ -19,16 +21,19 @@ class Investissement(models.Model):
     date_investissement = models.DateField(default=date.today)
     date_decompte = models.DateField(default=date.today)
     duree = models.PositiveSmallIntegerField(default=1)
-    # type = models.CharField(max_length=20, choices=TYPE_CHOICES, null=True)
+    type = models.CharField(max_length=20, choices=TYPE_CHOICES, null=True)
+
+    class Meta:
+        ordering = ['date_decompte']
 
     def __str__(self):
-        self.refresh_status_payements()
+        # self.refresh_status_payements()
 
         return f"investissement n° {self.id}"
 
     def supprimer_payements(self):
         from payement.models import Payement
-        self.refresh_status_payements()
+        # self.refresh_status_payements()
 
         payements = Payement.objects.filter(investissement=self)
         for payement in payements:
@@ -36,7 +41,7 @@ class Investissement(models.Model):
 
     def generer_payements(self):
         from payement.models import Payement
-        self.refresh_status_payements()
+#         # self.refresh_status_payements()
 
         base_date = self.date_decompte
         for i in range(self.duree):
@@ -57,27 +62,27 @@ class Investissement(models.Model):
             payement.save()
 
     def is_finish(self):
-        self.refresh_status_payements()
+        # self.refresh_status_payements()
 
         today = date.today()
         invest_end = incrementer_date(self.date_decompte, 30 * self.duree)
-        print(today > invest_end)
+
         return today > invest_end
 
     def payement_courant(self):
-        self.refresh_status_payements()
+        # self.refresh_status_payements()
 
         payement = self.payements.filter(status="NP").order_by('date').first()
         return payement
 
     def payements_termines(self):
-        self.refresh_status_payements()
+        # self.refresh_status_payements()
 
         payements = self.payements.filter(status__in=("VR", "RE", "EC")).order_by('date')
         return payements
 
     def refresh_status_payements(self):
-        payements = self.payements.filter(status__in=(None, "NP"))
+        payements = list(self.payements.filter(status="NP")) + list(self.payements.filter(status=None))
 
         for payement in payements:
             if payement.date < date.today():
@@ -88,4 +93,30 @@ class Investissement(models.Model):
                 payement.save()
 
     def bonus(self):
-        return float(self.montant) * POURCENTAGE
+        return float(self.montant) * POURCENTAGE_BONUS
+
+    def pourcentage_rsi(self):
+        return self.retour_sur_investissement() * 100 / float(self.montant)
+
+    def pourcentage(self):
+        aujourdhui = date.today()
+
+        if aujourdhui >= self.date_fin():
+            return 100
+
+        return int((aujourdhui - self.date_decompte).days * 100 / float(MONTH_LENGTH * self.duree))
+
+    def retour_sur_investissement(self):
+        return float(self.montant) * (1 + (POURCENTAGE_MENSUEL * self.duree))
+
+    def date_fin(self):
+        return incrementer_date(self.date_decompte, MONTH_LENGTH * self.duree)
+
+    def rang(self):
+        rank = 1
+        investissements = self.__class__.objects.filter(investisseur=self.investisseur)
+
+        for i in range(len(investissements)):
+            if investissements[i].id == self.id:
+                return i + 1
+        return rank
