6bc33b456d71cddea2612ec00e01b85c109748bd
samtou <christophe1999da@gmail.com>
2020-03-19 00:09:29 +0000

    Refactoring

--- payement/payements_state_updater.py
+++ payement/payements_state_updater.py
@@ -1,11 +1,5 @@
 from datetime import date
 
-# from apscheduler.schedulers.background import BackgroundScheduler
-#
-# scheduler = BackgroundScheduler()
-
-# @scheduler.scheduled_job('cron', day_of_week='*', hour=23, minute=33)
-# @scheduler.scheduled_job('interval', seconds=30)
 def refresh_status_job():
     from .models import Payement
 
@@ -20,15 +14,3 @@ def refresh_status_job():
         elif payement.debut_payement() < date.today():
             payement.status = "NP"
             payement.save()
-
-
-# sched.add_job(refresh_status_job, 'cron', day_of_week='*', hour=23, minute=21)
-
-
-# @sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
-# def scheduled_job():
-#     print('This job is run every weekday at 5pm.')
-
-
-# scheduler.start()
-# scheduler.shutdown(wait=False)
